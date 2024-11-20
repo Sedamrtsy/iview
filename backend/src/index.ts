a75b9cb4bb85ceae7a542caceaa9215a51190900
Sedamrtsy <seda.murutsoyy@gmail.com>
2024-11-21 00:59:16 +0300

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import question from './routes/question';
 import qpackage from './routes/package';
 import interview from './routes/interview';
 import candidates from './routes/candidates';
-import videoRoutes from './routes/video';  // Video yükleme rotasını ekleyin
+import videoRoutes from './routes/video';  // Video yükleme rotası
 import logoutRoute from './routes/logout';
 
 import { verifyToken } from './middleware/verifyToken';
@@ -17,18 +17,20 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const frontendURL = process.env.FRONTEND_URL || "http://localhost:3000"; // .env'den frontend URL'yi al
+const frontendURL = process.env.FRONTEND_URL || "https://iview-teal.vercel.app"; // .env'den frontend URL'yi al
 
 // MongoDB bağlantısını yap
 connectDB();
 
-// Middleware'ler
-// app.use(cors());
+// CORS Middleware'i
 app.use(cors({
-  origin: [frontendURL], // Ortam değişkeninden gelen URL'yi kullan
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: frontendURL, // Frontend URL'si
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // İzin verilen HTTP metotları
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Cookie veya token göndermek için gerekli
 }));
+
+app.options('*', cors()); // Preflight request'ler için CORS ayarını genişletin
 
 app.use(cookieParser()); // Cookie işlemleri için
 app.use(express.json());
@@ -36,19 +38,18 @@ app.use(express.urlencoded({ extended: true })); // Form verileri için
 
 // API rotaları
 app.use('/api', authRoute);
-
+app.use('/api', question);
+app.use('/api', qpackage);
+app.use('/api', interview);
+app.use('/api', candidates);
+app.use('/api', videoRoutes);  // Video yükleme için eklenen yeni rota
+app.use("/api", logoutRoute);
 
 // Korunan bir rota örneği (token doğrulama ile erişim kontrolü)
 app.get('/api/protected', verifyToken, (req: Request, res: Response) => {
   res.json({ message: 'Erişim başarılı', user: (req as any).user });
 });
-app.use('/api', question);
-app.use('/api', qpackage);
-app.use('/api', interview);
-app.use('/api', candidates);
-app.use('/api', videoRoutes);  // Video yükleme için eklenen yeni route
-app.use("/api", logoutRoute);
-app.use(express.urlencoded({ extended: true })); // Form verilerini işler
+
 // 404 Hatası için varsayılan yanıt
 app.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' });
