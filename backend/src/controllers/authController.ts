36a3d100f3c1f441bf6eaab68ad9a4224b723fdf
Sedamrtsy <seda.murutsoyy@gmail.com>
2024-11-22 19:36:00 +0300

    token

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -26,6 +26,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.status(200).json({
       msg: "Login successful",
       user: { email },
+      token, // Yanıta token'i de ekle
     });
   } catch (error) {
     console.error("Login işlemi sırasında bir hata oluştu:", error);
